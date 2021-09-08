8ee34cd6dbe9511ef3361f83dd180be576f70f5c
ITALO GABRIEL MESQUITA PEREI <20272538@cfp127aluno.local>
2021-09-08 11:31:15 -0300

    arumando rotas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("./database");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json())
 
